fb2f6c50f2470a295840f704e2433b7ea1e494c4
ViniciusMata <damata.vini@gmail.com>
2026-09-23 16:08:56 +0100

    Inclusão signal para gerar Bio automaticamente com IA, de momento desativado

--- cars/signals.py
+++ cars/signals.py
@@ -1,16 +1,9 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
+from openai_api.client import get_car_ai_bio
 
-
-@receiver(post_save, sender=Car)
-def car_post_save(sender, instance, **kwargs):
-    car_inventury_update()
-
-@receiver(post_delete, sender=Car)
-def car_post_delete(sender, instance, **kwargs):
-    car_inventury_update()
 
 def car_inventury_update():
     cars_count = Car.objects.all().count()
@@ -22,3 +15,25 @@ def car_inventury_update():
         cars_count = cars_count,
         cars_value = cars_value
     )
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        '''
+        ai_bio = get_car_ai_bio(
+            instance.model, instance.brand, instance.model_year
+        )
+        instance.bio = ai_bio
+        '''
+        instance.bio = 'Bio gerada automaticamente!'
+
+
+@receiver(post_save, sender=Car)
+def car_post_save(sender, instance, **kwargs):
+    car_inventury_update()
+
+
+@receiver(post_delete, sender=Car)
+def car_post_delete(sender, instance, **kwargs):
+    car_inventury_update()
