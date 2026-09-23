1c017e6c95bd28f79fa952612179cd7e836ba1d0
ViniciusMata <damata.vini@gmail.com>
2026-09-23 16:36:30 +0100

    Inclusão IA para gerar BIO automaticamente

--- openai_api/client.py
+++ openai_api/client.py
@@ -1,17 +1,25 @@
-import openai
+from openai import OpenAI
+
+client = OpenAI(
+        api_key = 'KEY_API'
+    )
 
 
 def get_car_ai_bio(model, brand, year):
-    prompt = ''''
+    message = ''''
         Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. 
         Fale coisas especificas desse modelo de carro.
         Descreva especificações ténicas sobre esse carro.
     '''
-    openai.api_key = ''
-    prompt = prompt.format(brand, model, year)
-    response = openai.Completion.create(
-        model = 'gpt-5.6-luna',
-        prompty = prompt,
+    message = message.format(brand, model, year)
+    response = client.chat.completions.create(
+        messages=[
+            {
+                'role': 'user',
+                'content': message
+            }
+        ],
         max_tokens = 100,
+        model = 'gpt-5-mini',
     )
-    return response['choices'][0]['text']
+    return response.choices[0].message.content
