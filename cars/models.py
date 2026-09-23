00a53085fae49e29b00bdb56c411bf753a13c503
ViniciusMata <damata.vini@gmail.com>
2026-09-23 13:35:01 +0100

    Incluido signal ao adicionar/exlcluir carro do carInventory

--- cars/models.py
+++ cars/models.py
@@ -18,6 +18,7 @@ class Car(models.Model):
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.model
@@ -32,7 +33,6 @@ class CarInventory(models.Model):
     class Meta:
         # Ordenação Decrescente
         ordering = ['-created_at']
-
-    
+  
     def __str__(self):
         return f'{self.cars_count} - {self.cars_value}'
--- cars/signals.py
+++ cars/signals.py
@@ -1,27 +1,24 @@
-from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
+from django.db.models.signals import post_save, post_delete
+from django.db.models import Sum
 from django.dispatch import receiver
-from cars.models import Car
-
-
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print('### PRE-SAVE ###')
-    print(instance)
+from cars.models import Car, CarInventory
 
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    print('### POST-SAVE ###')
-    print(instance)
-
-
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **kwargs):
-    print('### PRE-DELETE ###')
-    print(instance)
-
+    car_inventury_update()
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    print('### POST-DELETE ###')
-    print(instance)
+    car_inventury_update()
+
+def car_inventury_update():
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value = Sum('value')
+    )['total_value']
+
+    CarInventory.objects.create(
+        cars_count = cars_count,
+        cars_value = cars_value
+    )
