3f51735a6e9165e6f61b552716c1830cccbdedef
ViniciusMata <damata.vini@gmail.com>
2026-09-25 14:25:02 +0100

    Adicionado postgreSql para nova BD

--- app/settings.py
+++ app/settings.py
@@ -77,8 +77,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # BD com SQLite3
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        
+        # BD com Postgres
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carros',
+        'USER': 'postgres',
+        'PASSWORD': 'V012918m',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
