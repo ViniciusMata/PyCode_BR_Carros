965cbd6c9b32255f3dd47af6c48179ec3ee64de1
ViniciusMata <damata.vini@gmail.com>
2026-09-25 14:52:56 +0100

    Configuração para BD default e para ambiente DEV

--- app/settings.py
+++ app/settings.py
@@ -77,10 +77,6 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        # BD com SQLite3
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        
         # BD com Postgres
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'carros',
@@ -88,6 +84,10 @@ DATABASES = {
         'PASSWORD': 'V012918m',
         'HOST': 'localhost',
         'PORT': '5432',
+    },
+    'dev': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
